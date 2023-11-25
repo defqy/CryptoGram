0c3981fe57abbcc6aef985ff957e0759b7f987a1
defqy <t.speshilov@gmail.com>
2023-11-25 19:06:04 +0300

    Fixed bot.py

--- bot.py
+++ bot.py
@@ -2,6 +2,8 @@ from telebot import types
 from datetime import datetime
 import telebot
 
+print('Bot is working now!')
+
 bot = telebot.TeleBot('6571108770:AAEQwras-bZI_N49i6V4bRfOjHvZLvfJxQs')
 sender = telebot.TeleBot('6740671437:AAGR9Yekw2VlgUdAJdsBQWBkxIcjsXv8oCs')
 
@@ -14,13 +16,11 @@ def hello(message):
     keyboard = types.ReplyKeyboardMarkup(row_width = 1, resize_keyboard = True)
     crypted_message = types.KeyboardButton(text = 'Отправить зашифрованное сообщение')
     ID_user = types.KeyboardButton(text = "Узнать свой ID")
-    send_my_id = types.KeyboardButton(text = "Отправить свой ID")
-
     help = types.KeyboardButton(text = "🛟Помощь")
     escape = types.KeyboardButton(text = "⬇Скрыть")
 
     keyboard.row(crypted_message)
-    keyboard.row(ID_user, send_my_id, help)
+    keyboard.row(ID_user, help)
     keyboard.row(escape)
     bot.send_message(message.chat.id, "Привет, <b>{0.first_name}</b>\nДобро пожаловать в CryptoGram. Данный бот поможет Вашим сообщениям, которые проходят через Telegram оставаться зашифрованными. Переписка осуществляется в @defqy_cryptogram_chat_bot. Запустите его, чтобы получать сообщения".format(message.from_user, bot.get_me()), reply_markup = keyboard, parse_mode='HTML')
     bot.send_message(message.chat.id, 'Перед началом работы, убедитесь в том, что у вашего собеседника хоть раз был запущен данный бот (если нет, то перешлите ему ссылку на бота) или уберите его из заблокированных, чтобы собеседник получал сообщения')
@@ -35,13 +35,9 @@ def send_id(message):
 def start(message):
     if message.text == 'Узнать свой ID':
         bot.send_message(message.chat.id, f'Name - @{message.from_user.username}\nID - <code>{message.from_user.id}</code>\nFirst name - {message.from_user.first_name}\nLast name - {message.from_user.last_name}', parse_mode='HTML')
-    
-    elif message.text == 'Отправить свой ID':
-        bot.send_message(message.chat.id, 'Перешли мне любое сообщение твоего собеседника и я отправлю ему сообщение')
-        bot.register_next_step_handler(message, send_id)
 
     elif message.text == 'Отправить зашифрованное сообщение':
-        bot.send_message(message.from_user.id, "Напишите id своего получателя")
+        bot.send_message(message.from_user.id, "Напишите id своего получателя (напишите <code>stop</code> - если передумали)", parse_mode = 'HTML')
         bot.register_next_step_handler(message, get_id)
 
     elif message.text == '🛟Помощь':
@@ -56,13 +52,18 @@ def get_id(message):
     id_user = message.text
     try:
         current_datetime = datetime.now()
-        sender.send_message(id_user, f'<b>{message.from_user.first_name}</b> is writing you a new message 😉\n\nTime of typing: {current_datetime}\n\nName - @{message.from_user.username}\nID - <code>{message.from_user.id}</code>\nFirst name - {message.from_user.first_name}\nLast name - {message.from_user.last_name}', parse_mode = 'HTML')
+        current_datetime = current_datetime.strftime("%H:%M:%S %d.%m.%Y")
+        sender.send_message(id_user, f'<b>{message.from_user.first_name}</b> is writing you a new message 😉\n\nName - @{message.from_user.username}\nID - <code>{message.from_user.id}</code>\nFirst name - {message.from_user.first_name}\nLast name - {message.from_user.last_name}\n\nTime of typing: {current_datetime}', parse_mode = 'HTML')
         bot.send_message(message.from_user.id, 'Отправьте <strong>зашифрованное</strong> сообщение', parse_mode = 'HTML')
         bot.register_next_step_handler(message, get_msg)
     except:
-        bot.send_message(message.chat.id, 'Такого пользователя не существует, введите сообщение заново!')
-        bot.send_message(message.from_user.id, "Напишите id своего получателя")
-        bot.register_next_step_handler(message, get_id)
+        if id_user == 'stop':
+            bot.send_message(message.chat.id, 'Хорошо, поиск пользователя остановлен')
+
+        else:    
+            bot.send_message(message.chat.id, 'Такого пользователя не существует, введите сообщение заново!')
+            bot.send_message(message.from_user.id, "Напишите id своего получателя")
+            bot.register_next_step_handler(message, get_id)
 
 def get_msg(message):
     global msg
@@ -74,6 +75,8 @@ def get_init_vector(message):
     global init_vector
     init_vector = message.text
     current_datetime = datetime.now()
-    sender.send_message(id_user, f'Sending time: {current_datetime}\n\n<b>You have a new message: <code>{msg}</code></b>\nInitialization vector - <code>{init_vector}</code>\n\nSender:\nName - @{message.from_user.username}\nID - <code>{message.from_user.id}</code>\nFirst name - {message.from_user.first_name}\nLast name - {message.from_user.last_name}', parse_mode='HTML')
+    current_datetime = current_datetime.strftime("%H:%M:%S %d.%m.%Y")
+    sender.send_message(id_user, f'<b>You have a new message: <code>{msg}</code></b>\nInitialization vector - <code>{init_vector}</code>\n\nSender:\nName - @{message.from_user.username}\nID - <code>{message.from_user.id}</code>\nFirst name - {message.from_user.first_name}\nLast name - {message.from_user.last_name}\n\nSending time: {current_datetime}', parse_mode='HTML')
+    sender.send_message(message.from_user.id, f'<b>Your</b> message to <code>{id_user}</code>\n\n<b>Message: <code>{msg}</code></b>\nInitialization vector - <code>{init_vector}</code>\n\nSending time: {current_datetime}', parse_mode='HTML')
     bot.send_message(message.chat.id, 'Сообщение успешно отправлено!')
 bot.infinity_polling()
--- client.py
+++ client.py
@@ -2,7 +2,6 @@ import socket
 client = socket.socket(socket.AF_INET, socket.SOCK_STREAM)
 client.connect(("127.0.0.1", 8080)) #158.160.106.231
 receiver = socket.socket()
-
 while True:
     data = client.recv(1024)
     print(data.decode("utf-8"))
