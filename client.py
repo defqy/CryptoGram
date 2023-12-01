e52ffdcbeb8a816a5ff2fa1e2b020787bd30c889
defqy <t.speshilov@gmail.com>
2023-12-02 00:06:56 +0300

    Diffie-Hellman protocol
    Add a Diffie-Hellman protocol and deleting some files

--- client.py
+++
@@ -1,8 +0,0 @@
-import socket
-client = socket.socket(socket.AF_INET, socket.SOCK_STREAM)
-client.connect(("127.0.0.1", 8080)) #158.160.106.231 внешний ip
-receiver = socket.socket()
-while True:
-    data = client.recv(1024)
-    print(data.decode("utf-8"))
-    client.send(input().encode("utf-8"))
---
+++ diffie_hellman.py
@@ -0,0 +1,62 @@
+import random
+
+n = int(input('Sender - 1; Reciever - 2; Sender recieve - 3; Clear all files (without - keyDH.txt) - 0: '))
+if n == 1:
+    a = random.randint(100, 1000000)
+    g = random.randint(100, 1000000)
+    p = random.randint(100, 1000000)
+
+    A = g**a % p
+
+    with open('a.txt', 'w+') as f:
+            f.write(f'{a}')
+
+    with open('gpA.txt', 'w+') as f:
+        f.write(f'{g}\n{p}\n{A}')
+
+elif n == 2:
+    b = random.randint(100, 1000000)
+    with open('gpA.txt', 'r') as f:
+        q = f.readlines()
+        g = q[0]
+        g = int(g.replace('\n', ''))
+        p = q[1]
+        p = int(p.replace('\n', ''))
+        A = int(q[2])
+
+    B = g**b % p
+    K = A**b % p
+
+    with open('B.txt', 'w+') as f:
+        f.write(f'{B}')
+
+    with open('key_DH.txt', 'w+') as f:
+        f.write(f'{K}')
+
+elif n == 3:
+    with open('B.txt', 'r') as f:
+        B = f.readlines()
+        B = int(B[0])
+    
+    with open('gpA.txt', 'r') as f:
+        q = f.readlines()
+        g = q[0]
+        g = int(g.replace('\n', ''))
+        p = q[1]
+        p = int(p.replace('\n', ''))
+        A = int(q[2])
+
+    with open('a.txt', 'r') as f:
+        q = f.readlines()
+        a = int(q[0])
+
+    K = B**a % p
+    print(K)
+
+elif n == 0:
+    f = open('gpA.txt', 'r+')
+    f.truncate()
+    z = open('a.txt', 'r+')
+    z.truncate()
+    o = open('B.txt', 'r+')
+    o.truncate()
